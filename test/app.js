1a701d93fb54ba88ba5415cabc4b62e0612b2a49
Brian Fitzpatrick <bfitzpat@redhat.com>
2018-08-16 14:25:12 -0600

    adding mocha test for Java DSL template

--- test/app.js
+++ test/app.js
@@ -88,4 +88,36 @@ describe('generator-camel:app', function () {
     });
   });
 
+  describe('Should properly scaffold with default config for Java DSL', function () {
+
+    before(function () {
+      basicProps.name = 'MyAppMockJava';
+      basicProps.package = 'com.generator.mock.javadsl';
+      basicProps.camelVersion = '2.18.2';
+      basicProps.camelDSL = 'java';
+
+      return helpers.run(path.join(__dirname, '../app'))
+        .inTmpDir(function (dir) {
+          var done = this.async(); // `this` is the RunContext object.
+          fs.copy(path.join(__dirname, '../templates'), dir, done);
+        })
+        .withPrompts({ name: basicProps.name })
+        .withPrompts({ camelVersion: basicProps.camelVersion })
+        .withPrompts({ camelDSL: basicProps.camelDSL })
+        .withPrompts({ package: basicProps.package })
+        .toPromise();
+    });
+
+    it('Should create the basic structure', function () {
+      assert.file('pom.xml');
+      assert.file('README.md');
+      assert.file('src/main/java/com/generator/mock/javadsl/routes/CamelRoute.java');
+      assert.file('src/main/java/com/generator/mock/javadsl/routes/Launcher.java');
+    });
+
+    it('Should create pom.xml with default content', function () {
+      assert.fileContent('pom.xml', new RegExp('<groupId>' + basicProps.package + '</groupId>') );
+      assert.fileContent('pom.xml', new RegExp('<artifactId>' + basicProps.name + '</artifactId>') );
+    });
+  });
 });
